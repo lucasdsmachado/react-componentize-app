f9aa0116f948d8fab8d4bbebf66cc5f097e45b98
lucasdsmachado <brian.machado@ufba.br>
2021-12-23 10:27:03 -0300

    create header component

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { api } from "../services/api";
+import { Header } from "./Header";
 import { MovieCard } from "./MovieCard";
 
 interface GenreResponseProps {
@@ -27,25 +28,18 @@ export function Content({ selectedGenreId } : ContentProps) {
   // Complete aqui
 
   const [movies, setMovies] = useState<MovieProps[]>([]);
-  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
 
   useEffect(() => {
     api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
       setMovies(response.data);
     });
-
-    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
-      setSelectedGenre(response.data);
-    })
   }, [selectedGenreId]);
 
   
   return  (
    <div className="container"> 
-    <header>
-      <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-    </header>
     
+    <Header selectedGenreId={selectedGenreId}/>
     <main>
       <div className="movies-list">
         {movies.map(movie => (
